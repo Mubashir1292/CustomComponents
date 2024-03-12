e866174a960e62b1860a79e01447fc16b9c1edb0
Mubashir1292 <161022598+Mubashir1292@users.noreply.github.com>
2024-03-12 08:29:05 +0500

    Intermediate level of the dropdown component

--- src/components/dropDown/DropDown.jsx
+++ src/components/dropDown/DropDown.jsx
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 function DropDown({ options }) {
+  const [isOpen, setIsOpen] = useState(false);
+  const handleClick = (option) => {
+    setIsOpen(!isOpen);
+    console.log(option);
+  };
+
   const renderedOptions = options.map((item, index) => (
-    <div key={index}>
-      <div>{item.value}</div>
+    <div key={item.label}>
+      <div onClick={() => handleClick(item)} className="cursor-pointer">
+        {item.value}
+      </div>
     </div>
   ));
-  return <div>{renderedOptions}</div>;
+  return (
+    <div className="flex flex-col">
+      <div onClick={handleClick}>Select...</div>
+      {isOpen && <div> {renderedOptions}</div>}
+    </div>
+  );
 }
 
 export default DropDown;
